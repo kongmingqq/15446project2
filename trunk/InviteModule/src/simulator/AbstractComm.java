dfa9fd048e9d14f7a1359fcf771f1fc66c558661
foote.eric@gmail.com <foote.eric@gmail.com@4d2b8ffa-23db-11de-99f3-63f2d503d578>
2009-04-14 23:36:54 +0000

    Removed old InviteModule Project

--- trunk/InviteModule/src/simulator/AbstractComm.java
+++
@@ -1,9 +0,0 @@
-package simulator;
-
-public abstract class AbstractComm {
-	public abstract int getMyNumber();
-	public abstract void send(int identifier, Object obj);
-	public abstract int nextEventId();
-	public abstract String[] getPhoneBook();
-	public abstract int lookUp(String name);
-}
