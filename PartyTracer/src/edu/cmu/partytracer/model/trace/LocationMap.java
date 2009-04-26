1210613b3d93214234f84ad7fcc2ba9306dd1453
kongmingqq@gmail.com <kongmingqq@gmail.com@4d2b8ffa-23db-11de-99f3-63f2d503d578>
2009-04-26 19:40:09 +0000

    algorithm optimized

--- PartyTracer/src/edu/cmu/partytracer/model/trace/LocationMap.java
+++ PartyTracer/src/edu/cmu/partytracer/model/trace/LocationMap.java
@@ -3,6 +3,8 @@ package edu.cmu.partytracer.model.trace;
 import java.util.HashMap;
 import java.util.Set;
 
+import com.google.android.maps.GeoPoint;
+
 import edu.cmu.partytracer.Application;
 import edu.cmu.partytracer.bean.AggLocationBean;
 import edu.cmu.partytracer.bean.Location;
@@ -14,22 +16,41 @@ import edu.cmu.partytracer.bean.Location;
 
 public class LocationMap implements Cloneable {
 	private HashMap<String,LocationWithMove> map;
-
+	int maxLatitude;
+	int minLatitude;
+	int maxLongitude;
+	int minLongitude;
 	public LocationMap(AggLocationBean alb) {
 		map = new HashMap<String,LocationWithMove>();
+		int maxLat = Integer.MIN_VALUE;
+		int minLat = Integer.MAX_VALUE;
+		int maxLng = Integer.MIN_VALUE;
+		int minLng = Integer.MAX_VALUE;
 		for(Location l:alb.getLocations()) {
 			if(l.getId()==Application.MY_PHONE_ID) continue; //me is different
 			LocationWithMove lwm = new LocationWithMove(l.getId(),l.getLatitude(),l.getLongitude());
 			map.put(l.getId(), lwm);
+			maxLat = Math.max(maxLat, l.getLatitude());
+			minLat = Math.min(minLat, l.getLatitude());
+			maxLng = Math.max(maxLng, l.getLongitude());
+			minLng = Math.min(minLng, l.getLongitude());
 		}
+		maxLatitude = maxLat;
+		minLatitude = minLat;
+		maxLongitude = maxLng;
+		minLongitude = minLng;
 	}
 	
 	/**
 	 * constructor used to clone
 	 * @param m
 	 */
-	protected LocationMap(HashMap m) {
+	protected LocationMap(HashMap m, int maxLat, int minLat, int maxLng, int minLng) {
 		map = m;
+		maxLatitude = maxLat;
+		minLatitude = minLat;
+		maxLongitude = maxLng;
+		minLongitude = minLng;
 	}
 	
 	/**
@@ -38,10 +59,22 @@ public class LocationMap implements Cloneable {
 	 * @param alb
 	 */
 	public synchronized void update(AggLocationBean alb) {
+		int maxLat = Integer.MIN_VALUE;
+		int minLat = Integer.MAX_VALUE;
+		int maxLng = Integer.MIN_VALUE;
+		int minLng = Integer.MAX_VALUE;
 		for(Location l:alb.getLocations()) {
 			LocationWithMove lwm = new LocationWithMove(l.getId(),l.getLatitude(),l.getLongitude());
 			map.put(l.getId(), lwm);
+			maxLat = Math.max(maxLat, l.getLatitude());
+			minLat = Math.min(minLat, l.getLatitude());
+			maxLng = Math.max(maxLng, l.getLongitude());
+			minLng = Math.min(minLng, l.getLongitude());
 		}
+		maxLatitude = maxLat;
+		minLatitude = minLat;
+		maxLongitude = maxLng;
+		minLongitude = minLng;
 	}
 	
 	public synchronized LocationWithMove getLocationById(String id) {
@@ -50,6 +83,18 @@ public class LocationMap implements Cloneable {
 	
 	public synchronized Set<String> getIdSet() {
 		return map.keySet();
+	}
+	
+	public synchronized GeoPoint getCenterPoint() {
+		return new GeoPoint((maxLatitude+minLatitude)/2,(maxLongitude+minLongitude)/2);
+	}
+	
+	public synchronized int getLatitudeSpan() {
+		return maxLatitude-minLatitude;
+	}
+	
+	public synchronized int getLongitudeSpan() {
+		return maxLongitude-minLongitude;
 	}
 	
 	/**
@@ -64,6 +109,6 @@ public class LocationMap implements Cloneable {
 			LocationWithMove lwm = new LocationWithMove(l.getId(),l.getLatitude(),l.getLongitude());
 			newMap.put(l.getId(), lwm);
 		}
-		return new LocationMap(newMap);
+		return new LocationMap(newMap,maxLatitude,minLatitude,maxLongitude,minLongitude);
 	}
 }
