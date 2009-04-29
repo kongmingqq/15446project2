587c92864ca9c7b205f8d9ba75c2426e2961023e
foote.eric@gmail.com <foote.eric@gmail.com@4d2b8ffa-23db-11de-99f3-63f2d503d578>
2009-04-29 23:25:21 +0000

    Added functionality so that the user can manually set the current invite for the Map module to use

---
+++ PartyTracer/src/edu/cmu/partytracer/activity/invitation/SetCurrentInviteDialog.java
@@ -0,0 +1,56 @@
+package edu.cmu.partytracer.activity.invitation;
+
+import edu.cmu.partytracer.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.view.View;
+
+public class SetCurrentInviteDialog extends Activity implements View.OnClickListener, OnCheckedChangeListener{
+	private String selected;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setcurrent);
+        selected = "";
+        
+        Button done = (Button) findViewById(R.id.donesetting);
+        done.setOnClickListener(this);
+        
+        RadioGroup inviteList = (RadioGroup) findViewById(R.id.allactiveinvites);
+        inviteList.setOnCheckedChangeListener(this);
+        getEventNames(this.getIntent(), inviteList);
+	}
+
+	private void getEventNames(Intent iData, RadioGroup list)
+	{
+		int item = 0;
+		
+		while(iData.hasExtra(ViewInvitesDialog.INVITE + item))
+		{
+			RadioButton nextInvite = new RadioButton(this);
+			nextInvite.setText(iData.getStringExtra(ViewInvitesDialog.INVITE));
+			list.addView(nextInvite);
+			
+			item++;
+		}
+	}
+	
+	public void onClick(View v) {
+		Intent setIntent = new Intent();
+		
+		setIntent.putExtra(ViewInvitesDialog.IID, selected);
+
+		setResult(RESULT_OK, setIntent);
+		finish();
+	}
+
+	public void onCheckedChanged(RadioGroup arg0, int arg1) {
+		RadioButton invite = (RadioButton)findViewById(arg1);
+		selected = invite.getText().toString();
+	}
+}
