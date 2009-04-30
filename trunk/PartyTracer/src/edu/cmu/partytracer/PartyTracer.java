037d581c9869ccb52de5daae6c2211efbf80f392
kongmingqq@gmail.com <kongmingqq@gmail.com@4d2b8ffa-23db-11de-99f3-63f2d503d578>
2009-04-30 01:45:08 +0000

    good version

--- trunk/PartyTracer/src/edu/cmu/partytracer/PartyTracer.java
+++ trunk/PartyTracer/src/edu/cmu/partytracer/PartyTracer.java
@@ -2,11 +2,13 @@ package edu.cmu.partytracer;
 
 import edu.cmu.partytracer.activity.trace.*;
 import edu.cmu.partytracer.activity.invitation.*;
+import edu.cmu.partytracer.activity.invitation.testing.TestDataGenerator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class PartyTracer extends Activity implements View.OnClickListener{
     /** Called when the activity is first created. */
@@ -16,9 +18,11 @@ public class PartyTracer extends Activity implements View.OnClickListener{
         setContentView(R.layout.main);
         Button moduleTrace = (Button) findViewById(R.id.module_trace);
         Button moduleInvite = (Button) findViewById(R.id.module_invite);
+        Button testTraceToggle = (Button) findViewById(R.id.test_tracetoggle);
         
         moduleInvite.setOnClickListener(this);
         moduleTrace.setOnClickListener(this);
+        testTraceToggle.setOnClickListener(this);
     }
 
 	public void onClick(View v) {
@@ -37,6 +41,17 @@ public class PartyTracer extends Activity implements View.OnClickListener{
 		{
 			Intent i = new Intent(this, InviteModule.class);
 			startActivity(i);
+		}
+		else if(v.getId() == R.id.test_tracetoggle)
+		{
+			if(Application.CURRENT_PARTY_ID == null) {
+				Application.CURRENT_PARTY_ID = "1000";
+				Toast.makeText(getBaseContext(), "Party id set to 1000", Toast.LENGTH_SHORT).show();
+			} else if(Application.CURRENT_PARTY_ID.equals("1000")) {
+				Application.CURRENT_PARTY_ID = null;
+				Toast.makeText(getBaseContext(), "Party id set to NULL", Toast.LENGTH_SHORT).show();
+			}
+			
 		}
 		
 	}
