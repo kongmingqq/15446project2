850712fa6ad7aa0242990f4d96658671581b42d7
foote.eric@gmail.com <foote.eric@gmail.com@4d2b8ffa-23db-11de-99f3-63f2d503d578>
2009-05-01 05:23:09 +0000

    Updated the server IP

--- trunk/PartyTracer/src/edu/cmu/partytracer/Application.java
+++ trunk/PartyTracer/src/edu/cmu/partytracer/Application.java
@@ -9,7 +9,7 @@ import edu.cmu.partytracer.model.trace.CacheQueue;
  *
  */
 public final class Application {
-	public static volatile String SERVER_IP = "127.0.0.1";//;"128.237.250.136";//
+	public static volatile String SERVER_IP = "128.237.240.8";
 	
 	public static volatile Thread TRACE_RECEIVE_THREAD = null;
 	public static volatile Thread TRACE_SEND_THREAD = null;
